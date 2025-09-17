fc596c662dc122607c8b59a590041525b1a310be
imarksky95 <imarksky@gmail.com>
2025-09-17 09:33:55 +0000

    commit

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://accountingsystemv101-ehejf4b9bbhredbd.canadacentral-01.azurewebsites.net/api/auth';
+const API_URL = 'http://127.0.0.1:5000/api/auth';
 
 export async function register(username: string, password: string, role_id: number) {
   return axios.post(`${API_URL}/register`, { username, password, role_id });
