3d9c933e25acdf8a421694d3b4e13e9e87f7f339
imarksky95 <imarksky@gmail.com>
2025-09-22 16:35:25 +0000

    chore(deploy): publish frontend build to gh-pages

--- frontend/src/pages/ARManagement.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const ARManagement: React.FC = () => <h2>AR Management Module</h2>;
-export default ARManagement;
--- frontend/src/pages/BankingManagement.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const BankingManagement: React.FC = () => <h2>Banking Management Module</h2>;
-export default BankingManagement;
